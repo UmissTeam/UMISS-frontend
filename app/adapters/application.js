af73c30df0f5258dff0e11bb5f2874849dd4f2e0
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-05-06 19:39:05 -0300

    Preventing for unauthenticated requests

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
 import ENV from 'umiss-frontend/config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.host,
-  namespace: 'api'
+  namespace: 'api',
+  authorizer: 'authorizer:drf-token-authorizer'
 });
