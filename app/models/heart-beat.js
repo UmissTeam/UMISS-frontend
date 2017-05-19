74c61db231438c60a5fbedf30fc048da4413e77a
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-05-18 21:27:56 -0300

    Updating models based in api-backend changes

--- app/models/heart-beat.js
+++ app/models/heart-beat.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   owner: DS.attr('string'),
   created: DS.attr('string'),
-  beats: DS.attr('number')
+  beats: DS.attr('number'),
+  is_critical: DS.attr('boolean')
 });
