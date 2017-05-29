d3d7b9bc560c4f0291b09672dea917508e5d7a42
Dylan Guedes <djmgguedes@gmail.com>
2017-05-28 22:14:51 -0300

    Adds bootstrap integration
    Signed-off-by: Dylan Guedes <djmgguedes@gmail.com>

--- config/environment.js
+++ config/environment.js
@@ -30,7 +30,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.host = 'http://127.0.0.1:8000'
+    // ENV.host = 'http://127.0.0.1:8000'
+    ENV.host = 'http://104.196.160.171';
     ENV['ember-simple-auth'] = {
       authenticationRoute: 'login',
       routeAfterAuthentication: '/',
