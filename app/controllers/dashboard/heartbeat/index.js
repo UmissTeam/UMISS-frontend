7425d8494354b144d9e69cf452f322a129035229
Dylan Guedes <djmgguedes@gmail.com>
2017-05-29 21:19:32 -0300

    Get values from end
    Signed-off-by: Dylan Guedes <djmgguedes@gmail.com>

--- app/controllers/dashboard/heartbeat/index.js
+++ app/controllers/dashboard/heartbeat/index.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   data: Ember.computed('model', function() {
     return {
-      labels: this.get('model').mapBy('beats'),
+      labels: this.get('model').mapBy('beats').slice(-10),
       datasets: [{
         label: 'test',
-        data: this.get('model').mapBy('beats')
+        data: this.get('model').mapBy('beats').slice(-10)
       }]
     }
   }).property('model')
